674390c7cc6a62ed747ff79fab84744165e2db16
Mulekotd <joaopedro.gama0675@gmail.com>
2026-05-02 01:01:12 -0300

    Remove fly.io

--- dockerfiles/configfiles/django/entrypoint.sh
+++ dockerfiles/configfiles/django/entrypoint.sh
@@ -8,8 +8,8 @@ echo "Collecting statics..."
 python manage.py collectstatic --noinput
 
 echo "Initializing Gunicorn..."
-exec python -m gunicorn DjangoAbsortech.wsgi:application \
-     --bind 0.0.0.0:8000 \
+exec python -m gunicorn DjangoAbsortech.wsgi:application \\
+     --bind 0.0.0.0:${PORT:-8000} \\
      --workers ${GUNICORN_WORKERS:-3} \
      --threads ${GUNICORN_THREADS:-3} \
      --timeout 30
