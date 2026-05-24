18dd22530f2fbe3a42949bd4a87be27ab2a88809
Gabriel Sales Rodrigues <88689778+gaabpng@users.noreply.github.com>
2026-05-24 13:30:39 -0300

    Rename leituras endpoint to dispenser status

--- backend/DjangoAbsortech/urls.py
+++ backend/DjangoAbsortech/urls.py
@@ -22,7 +22,7 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/leituras/', views.obter_leituras, name='obter_leituras'), # GET para Frontend
+    path('api/leituras/', views.obter_status_atual_dispensers, name='obter_status_atual_dispensers'), # GET para Frontend
     path('api/leituras/endpoint_esp/', views.endpoint_leituras_esp32, name='endpoint_leituras_esp32'), # POST para o ESP32
 ]
 
